34b46ec79a2c059965e6411cd4be5635030a2bb5
Karanbharthi23 <goswamikaran5679@gmail.com>
2025-02-09 12:49:31 +0000

    datatypes

---
+++ javaacode/dataty.js
@@ -0,0 +1,26 @@
+let namwe="Karan"
+let n=1212
+console.log(typeof namwe)
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+
+console.log("Karan Goswami")
+
+
+let name = "Karan"
+let age = 19
+let isLoggedIn = true
+let state =null;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
