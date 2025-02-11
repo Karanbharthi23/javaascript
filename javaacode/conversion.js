e31cc7354c11c860222f5aed45370800832c5c2b
Karanbharthi23 <goswamikaran5679@gmail.com>
2025-02-11 13:33:02 +0000

    values conversion

--- javaacode/conversion.js
+++ javaacode/conversion.js
@@ -2,17 +2,37 @@ console.log("hello world!!");
 let numbber=10// this is an number
 let strg='hello github '//this is an string 
 let bul=true;//boolean 0 for false and 1 for  true
-if(bul!=null)//use of boolean
-{
-    console.log(typeof(strg));//type defined
-    console.log(strg);//writted
-    console.log(typeof numbber);//type defind
-    console.log(numbber);//writted
-}
- let n=Number(strg)//conversion string to int/number
-let s=String(numbber)//conversion number to string
-    console.log(typeof s);
-    console.log(s);
-    console.log(typeof n);
-    console.log(n)
+// if(bul!=null)//use of boolean
+// {
+//     console.log(typeof(strg));//type defined
+//     console.log(strg);//writted
+//     console.log(typeof numbber);//type defind
+//     console.log(numbber);//writted
+// }
+//  let n=Number(strg)//conversion string to int/number
+// let s=String(numbber)//conversion number to string
+    // console.log(typeof s);
+    // console.log(s);
+    // console.log(typeof n);
+    // console.log(n)
+let val=122
+let neg=-val
+console.log(neg);
+let num1=10;
+let num2=20;
+let num3=num1+num2;
+console.log(num1+"+"+num2+"="+num3);
+let str="Karan "
+let str1="Goswami"
+let str3=str+str1;
+console.log(str3);
+console.log("1"+2+2);
+console.log(2+3+13);
+console.log(2+"2");
+console.log(585/12*3)
+
+
+
+
+
 
