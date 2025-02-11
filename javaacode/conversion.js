8220296ed979a345df2bb34c721c5da372aa381c
Karanbharthi23 <goswamikaran5679@gmail.com>
2025-02-11 12:53:21 +0000

    conversion using data types

---
+++ javaacode/conversion.js
@@ -0,0 +1,18 @@
+console.log("hello world!!");
+let numbber=10// this is an number
+let strg='hello github '//this is an string 
+let bul=true;//boolean 0 for false and 1 for  true
+if(bul!=null)//use of boolean
+{
+    console.log(typeof(strg));//type defined
+    console.log(strg);//writted
+    console.log(typeof numbber);//type defind
+    console.log(numbber);//writted
+}
+ let n=Number(strg)//conversion string to int/number
+let s=String(numbber)//conversion number to string
+    console.log(typeof s);
+    console.log(s);
+    console.log(typeof n);
+    console.log(n)
+
