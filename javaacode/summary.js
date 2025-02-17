5fa14e5fc7bea0e375969e8965b7d910fcd96f19
Karanbharthi23 <goswamikaran5679@gmail.com>
2025-02-17 11:31:04 +0000

    summary trial

---
+++ javaacode/summary.js
@@ -0,0 +1,36 @@
+// console.log("hello world !");
+// console.log(null>0)
+// console.log(null ==0)
+// console.log(null<=0);
+// console.log(undefined==0);
+// console.log(1!=null);
+// console.log(undefined<0);
+// console.log("2"==2);
+// Primitive data types
+const Number=122;
+const Symbl=Symbol('123');
+const ANO=Symbol('12222');
+let unf;
+const login= false;
+if(login==false)
+{
+console.log(Symbl===ANO)
+}
+
+const xyz=["KARAN","RAJ","VIVEK"]
+console.log(xyz[0]);
+const a = {
+    NAME : "KARAN",
+    ID: 1,
+}
+console.log(a["NAME"]);
+const ret= function f1 ()
+{
+    
+    console.log("Hello World");
+   // return ret1;
+}
+//console.log(ret);   
+     
+
+
