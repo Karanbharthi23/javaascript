f1520d9ec9d0d7828f05ca9def1f5b180de692d8
Karanbharthi23 <goswamikaran5679@gmail.com>
2025-03-03 12:45:05 +0000

    this is all maths, string and date function

---
+++ javaacode/functions_07.js
@@ -0,0 +1,14 @@
+// const balance=400
+// console.log(balance)
+// const num=new Number(1000)
+// console.log(num);
+// console.log(Math.random()*10);
+// const min=10
+// const max=20                            
+// console.log(Math.ceil(Math.random() * (min - max + 1) + min));
+let mydate=new Date()
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.getMonth());
